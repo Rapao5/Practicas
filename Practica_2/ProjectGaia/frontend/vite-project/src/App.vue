3d612e4666b538f4dc867daed71a975def7dd13e
Rapao5 <2061045@alu.murciaeduca.es>
2026-03-10 16:38:42 +0100

    tarea dos modulo 2.2

--- Practica_2/ProjectGaia/frontend/vite-project/src/App.vue
+++ Practica_2/ProjectGaia/frontend/vite-project/src/App.vue
@@ -1,30 +1,45 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, computed } from 'vue' 
+
+const sensors = ref([]) 
+const searchQuery = ref('')
+
+const fetchSensors = async() =>{
+  const API_URL = 'https://localhost:7016/api/sensors';
+  const response = await fetch(API_URL);
+  const data = await response.json();
+  sensors.value = data;
+}
+onMounted(()=>{
+  fetchSensors();
+})
+
+const searchSensor = computed(()=>{
+  const search = searchQuery.value.toLowerCase();
+
+  return sensors.value.filter(sensor =>{
+      return sensor.zona.toLowerCase().includes(search);
+  });
+});
 </script>
 
 <template>
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <input 
+      v-model="searchQuery" 
+      placeholder="Filtrar sensores..." 
+    />
+
+    <ul>
+      <li v-for="sensor in searchSensor" :key="sensor.id">
+        {{ sensor.zona }}: {{ sensor.estado }}: {{ sensor.temperatura }}
+      </li>
+    </ul>
+    
+    <p v-if="searchSensor.length === 0">No se encontraron sensores.</p>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style>
+
 </style>
